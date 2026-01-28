8c8d45f2a4a3e35cae1977ca328fd0d089f7b18f
Julio Bastidas <juliobas@gmail.com>
2026-01-28 14:22:38 -0400

    Remove debug log from authorization callback in auth configuration

--- auth.config.ts
+++ auth.config.ts
@@ -6,7 +6,6 @@ export const authConfig = {
     },
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
-            console.log(auth);
             const isLoggedIn = !!auth?.user;
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
             
